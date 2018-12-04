14128a3d9e33038b80ae8b928def94bd22e1ae79
Masatomi KINO <masatomix@ki-no.org>
2018-12-04 16:25:20 +0900

    V[x] = V[X^2] -m^2 を追加

--- chapter05_1d.py
+++ chapter05_1d.py
@@ -4,24 +4,30 @@
 from matplotlib import animation as ani
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
 import sys
 
 
 def main(args):
-    # 取り得る値
-    x_set = np.array([1, 2, 3, 4, 5, 6])
+    # 確率変数(取り得る値と、確率関数のセット) を返す関数
+    def F():
+        _x_set = np.array([1, 2, 3, 4, 5, 6])
 
-    def f(x):
-        if x in x_set:
-            return x / np.sum(x_set)
-        else:
-            return 0
+        # _x_set = np.arange(1,7)
+
+        def _f(x):
+            if x in _x_set:
+                return x / np.sum(_x_set)
+            else:
+                return 0
+
+        return _x_set, _f
 
     # 確率変数を定義
-    X = [x_set, f]
+    X = F()
+    x_set, f = X
 
-    # 確率を表示
+    # 実際の確率を表示
     prob = np.array([f(x_k) for x_k in x_set])
     # print(dict(zip(x_set,prob)))
 
@@ -30,9 +36,13 @@ def main(args):
 
     # 期待値の定義
     mean = np.sum([x_k * f(x_k) for x_k in x_set])
+    # Σ ( x_k * f(x_k) )
+    # x_k in x_set で。
 
     # 分散の定義
     var = np.sum([(x_k - mean) ** 2 * f(x_k) for x_k in x_set])
+    # Σ ( (x_k - mean)^2 * f(x_k) )
+    # x_k in x_set で。
 
     # 無作為抽出の実施結果
     sample = np.random.choice(x_set, 10000000, p=prob)
@@ -45,31 +55,21 @@ def main(args):
     print(f"関数での出力: {V(X):.3f}")
     print(f"無作為抽出の分散値: {np.var(sample):.3f}")
 
+    # ついでに、正規化してみる
     def s(x):
         return (x - mean) / np.sqrt(var)
 
-    print(f"{E(X, g=lambda x: s(x)):.3f}")
-    print(f"{V(X, g=lambda x: s(x)):.3f}")
+    print("正規化した平均:", f"{E(X, g=lambda x: s(x)):.3f}")
+    print("正規化した分散:", f"{V(X, g=lambda x: s(x)):.3f}")
 
-    fig = plt.figure()
-
-    ax1 = fig.add_subplot(1, 1, 1)
-    ax1.bar(x_set, prob)
-
-    ax1.set_xlabel('取り得る値')
-    ax1.set_ylabel('確率')
-
-    plt.tight_layout()  # タイトルの被りを防ぐ
-
-    # グラフに情報を表示
-    plt.show()
+    plot_prob(X)
 
 
 def E(X, g=lambda x: x):
     """
     離散型確率変数の、期待値の定義
-    :param X:
-    :param g:
+    :param X: 確率変数
+    :param g: E[g[x]] とかって渡すときの関数
     :return:
     """
     x_set, f = X
@@ -77,14 +77,45 @@ def E(X, g=lambda x: x):
 
 
 def V(X, g=lambda x: x):
+    """
+    離散型確率変数の、分散の定義
+    :param X: 確率変数
+    :param g: V[g[x]] とかって渡すときの関数
+    :return:
+    """
     mean = E(X, g)
     return E(X, g=lambda x: (g(x) - mean) ** 2)
 
 
 # def V(X, g=lambda x: x):
+#     mean = E(X, g)   # E[X]
+#     mean2 = E(X, g=lambda x: g(x) ** 2)  # E[X^2]
+#     return mean2 - mean ** 2
+
+# def V(X, g=lambda x: x):
 #     x_set, f = X
 #     mean = E(X, g)
 #     return np.sum([(g(x_k) - mean) ** 2 * f(x_k) for x_k in x_set])
+
+
+def plot_prob(X):
+    x_set, f = X
+    prob = np.array([f(x_k) for x_k in x_set])
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(1, 1, 1)
+    ax1.bar(x_set, prob, label='prob')
+    ax1.vlines(E(X), 0, 1, label='mean')
+    ax1.set_xticks(np.append(x_set, E(X)))
+    ax1.set_ylim(0, prob.max() * 1.2)
+
+    ax1.set_xlabel('取り得る値')
+    ax1.set_ylabel('確率')
+
+    plt.tight_layout()  # タイトルの被りを防ぐ
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
